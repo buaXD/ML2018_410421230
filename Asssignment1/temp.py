2d6e1dd2c9bb1ac9cef773f9d51214e82082f73b
buaXD <s920043@gmail.com>
2018-05-02 00:20:22 +0800

    load basic

--- Asssignment1/temp.py
+++ Asssignment1/temp.py
@@ -5,13 +5,18 @@ import matplotlib
 
 key1=cv2.imread(r'..\ML2018_410421230\Asssignment1\keys\key1.png',0)
 key2=cv2.imread(r'..\ML2018_410421230\Asssignment1\keys\key2.png',0)
+inp=cv2.imread(r'..\ML2018_410421230\Asssignment1\I.png',0)
+E=cv2.imread(r'..\ML2018_410421230\Asssignment1\E.png',0)
 
+def printbasic():
+    cv2.namedWindow('key1',cv2.WINDOW_AUTOSIZE)
+    cv2.imshow('key1',key1)
+    cv2.namedWindow('key2',cv2.WINDOW_AUTOSIZE)
+    cv2.imshow('key2',key2)
+    cv2.namedWindow('I',cv2.WINDOW_AUTOSIZE)
+    cv2.imshow('I',inp)
+    cv2.namedWindow('E',cv2.WINDOW_AUTOSIZE)
+    cv2.imshow('E',E)
+    k=cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-cv2.namedWindow('key1',cv2.WINDOW_AUTOSIZE)
-cv2.imshow('key1',key1)
-
-cv2.namedWindow('key2',cv2.WINDOW_AUTOSIZE)
-cv2.imshow('key2',key2)
-
-k=cv2.waitKey(0)
-cv2.destroyAllWindows()
