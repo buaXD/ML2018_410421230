8ec07217a7c3174084fc857c6c75c0f1fc779fbb
buaXD <s920043@gmail.com>
2018-07-04 18:56:02 +0800

    normalization finished

--- Assignment2/train.py
+++ Assignment2/train.py
@@ -5,10 +5,19 @@ from keras.datasets import mnist
 
 (x_train,y_train),(x_test,y_test)=mnist.load_data()
 
+'''
 img=x_train[0]
-
 plt.imshow(img,cmap=plt.cm.gray)
-
 plt.show()
+'''
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+x_train/=255    #normalization
+x_test/=255
+
+
+
 
 #print(x_train.shape,x_test.shape)
