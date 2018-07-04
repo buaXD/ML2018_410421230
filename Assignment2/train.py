3b98382ec65fd084e1700845b22a0cae017603a5
buaXD <s920043@gmail.com>
2018-07-04 21:00:46 +0800

    start doing model

--- Assignment2/train.py
+++ Assignment2/train.py
@@ -6,25 +6,27 @@ from sklearn.decomposition import PCA
 
 (x_train,y_train),(x_test,y_test)=mnist.load_data()
 
-'''
-img=x_train[0]
-plt.imshow(img,cmap=plt.cm.gray)
-plt.show()
-'''
+#print(type(x_train))
 
-x_train=x_train.reshape(60000,28*28).astype("float32")/255
+def showimg(img):   #img=x_train[0]
+    print(img.shape)
+    if(img.shape[0]==200):
+        img=img.reshape(10,20)
+        print(img.shape)
+    plt.imshow(img,cmap=plt.cm.gray)
+    plt.show()
+
+x_train=x_train.reshape((60000,28*28)).astype("float32")/255 #normalization
 x_test=x_test.reshape(10000,28*28).astype("float32")/255
-
-x_train/=255    #normalization
-x_test/=255
-
-print(x_train.shape)
 
 pca=PCA(200,True,True)
 x_train=pca.fit_transform(x_train)
 x_test=pca.transform(x_test)
 
-print(x_train.shape)
+print(x_train.shape,x_test.shape)
+
+#showimg(x_train[0])
 
 
-#print(x_train.shape,x_test.shape)
+y_train = keras.utils.to_categorical(y_train,10)
+y_test = keras.utils.to_categorical(y_test,10)
