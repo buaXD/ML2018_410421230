354107a98d2db5c164d33a87ac5f6525b85b26e6
buaXD <s920043@gmail.com>
2018-07-04 18:38:49 +0800

    test to look pic

--- Assignment2/train.py
+++ Assignment2/train.py
@@ -1,6 +1,14 @@
 import keras
+import numpy as np
+import matplotlib.pyplot as plt
 from keras.datasets import mnist
 
 (x_train,y_train),(x_test,y_test)=mnist.load_data()
 
-print(x_train.shape,x_test.shape)
+img=x_train[0]
+
+plt.imshow(img,cmap=plt.cm.gray)
+
+plt.show()
+
+#print(x_train.shape,x_test.shape)
